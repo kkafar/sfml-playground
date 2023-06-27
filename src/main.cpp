43773f1bf7ec38fb5351c1f911c17dc530b10a57
Kacper Kafara <kacperkafara@gmail.com>
2023-06-27 16:36:00 +0200

    Setup GLOG

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,17 @@
 #include "SFML/Window/Event.hpp"
 #include <iostream>
 #include <SFML/Graphics.hpp>
+#include <glog/logging.h>
+
+void initLogging(const char *argv0) {
+  fLB::FLAGS_logtostdout = true;
+  google::InitGoogleLogging(argv0);
+}
 
 int main (int argc, char *argv[]) {
+  initLogging(argv[0]);
+  LOG(INFO) << "Running application";
+
   sf::RenderWindow window(sf::VideoMode(600, 600), "SFML works!");
   sf::CircleShape circle_shape(100.f);
   circle_shape.setFillColor(sf::Color::Green);
