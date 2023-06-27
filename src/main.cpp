7837ea1ccf301beb25640b282f4bef2f469c3729
Kacper Kafara <kacperkafara@gmail.com>
2023-06-27 14:47:20 +0200

    chore: prepare setup for SFML

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,25 @@
+#include "SFML/Graphics/CircleShape.hpp"
+#include "SFML/Window/Event.hpp"
 #include <iostream>
+#include <SFML/Graphics.hpp>
 
 int main (int argc, char *argv[]) {
-  std::cout << "Hello world\n";
+  sf::RenderWindow window(sf::VideoMode(600, 600), "SFML works!");
+  sf::CircleShape circle_shape(100.f);
+  circle_shape.setFillColor(sf::Color::Green);
+
+  while (window.isOpen()) {
+    sf::Event event;
+    while (window.pollEvent(event)) {
+      if (event.type == sf::Event::Closed) {
+        window.close();
+      }
+    }
+
+    window.clear();
+    window.draw(circle_shape);
+    window.display();
+  }
+
   return 0;
 }
