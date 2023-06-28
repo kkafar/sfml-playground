27d11966c0b46dc67773c80e09a5c4564ade7eed
Kacper Kafara <kacperkafara@gmail.com>
2023-06-28 13:01:51 +0200

    Move Chessboard implementation to separate cpp file

--- src/Chessboard.hpp
+++ src/Chessboard.hpp
@@ -22,67 +22,20 @@ public:
   constexpr static sf::Uint32 HEIGHT = 8;
 
   Chessboard(const sf::Uint32 width_px, const sf::Uint32 height_px,
-             const TextureStore &texture_store)
-      : m_width_px(width_px), m_height_px(height_px),
-        m_player_white(PlayerKind::White, width_px / WIDTH, height_px / HEIGHT, texture_store),
-        m_player_black(PlayerKind::Black, width_px / WIDTH, height_px / HEIGHT, texture_store) {
-    LOG(INFO) << "Chessboard ctor";
+             const TextureStore &texture_store);
 
-    sf::Uint32 tile_width_px = width_px / WIDTH;
-    sf::Uint32 tile_height_px = height_px / HEIGHT;
-
-    for (sf::Uint32 i = 0; i < HEIGHT; ++i) {
-      for (sf::Uint32 j = 0; j < WIDTH; ++j) {
-        sf::RectangleShape &field = m_fields[i][j];
-        const sf::Color tile_color =
-            (i + j) % 2 == 0 ? sf::Color::White : sf::Color::Black;
-        field.setFillColor(tile_color);
-        field.setPosition(j * tile_width_px, i * tile_height_px);
-        field.setSize(sf::Vector2f(tile_width_px, tile_height_px));
-      }
-    }
-  }
-
-  void resize(const sf::Uint32 width_px, const sf::Uint32 height_px) {
-    if (m_width_px == width_px && m_height_px == height_px) {
-      return;
-    }
-
-    m_width_px = width_px;
-    m_height_px = height_px;
-
-    sf::Uint32 tile_width_px = m_width_px / WIDTH;
-    sf::Uint32 tile_height_px = m_height_px / HEIGHT;
-
-    for (sf::Uint32 i = 0; i < HEIGHT; ++i) {
-      for (sf::Uint32 j = 0; j < WIDTH; ++j) {
-        sf::RectangleShape &field = m_fields[i][j];
-        field.setSize(sf::Vector2f(tile_width_px, tile_height_px));
-        field.setPosition(j * tile_width_px, i * tile_height_px);
-      }
-    }
-
-    m_player_black.resize(width_px, height_px);
-    m_player_white.resize(width_px, height_px);
-  }
+  void resize(const sf::Uint32 width_px, const sf::Uint32 height_px);
 
 protected:
-  virtual void draw(sf::RenderTarget &target, sf::RenderStates states) const {
-    for (const auto &field_row : m_fields) {
-      for (const auto &field : field_row) {
-        target.draw(field, states);
-      }
-    }
-
-    target.draw(m_player_white);
-    target.draw(m_player_black);
-  }
+  virtual void draw(sf::RenderTarget &target, sf::RenderStates states) const;
 
 private:
   std::array<std::array<sf::RectangleShape, WIDTH>, HEIGHT> m_fields;
 
   sf::Uint32 m_width_px;
   sf::Uint32 m_height_px;
+  sf::Uint32 m_tile_width_px;
+  sf::Uint32 m_tile_height_px;
 
   PlayerPieces m_player_white;
   PlayerPieces m_player_black;
