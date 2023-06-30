d322a93f86f83525576d22ba8badad43a34a6a54
Kacper Kafara <kacperkafara@gmail.com>
2023-06-30 15:51:11 +0200

    fix: pawn moves like a pawn now (#31)

--- src/Model/PawnMovePolicy.hpp
+++ src/Model/PawnMovePolicy.hpp
@@ -9,7 +9,7 @@
 
 class PawnMovePolicy : public MovePolicy {
 public:  
-  PawnMovePolicy(PlayerKind piece_color) : MovePolicy(PieceKind::Pawn), m_pawn_color(piece_color) {}
+  PawnMovePolicy(PlayerKind piece_color);
 
   /// Calculates all valid move-to positions for the figure from the given position
   /// and puts the result in provided vector. Make sure (or not) that the vector is empty
@@ -18,16 +18,8 @@ public:
 
 private:
   PlayerKind m_pawn_color;
-  constexpr static std::array<BoardPosition, 1> s_white_pawn_moves{
-    {1, 0}
-  };
-  constexpr static std::array<BoardPosition, 1> s_black_pawn_moves{
-    {-1, 0} 
-  };
-
-  void whitePawnMoves(const Piece &piece, Chessboard &board, std::vector<Move> &result);
-  void blackPawnMoves(const Piece &piece, Chessboard &board, std::vector<Move> &result);
-  void pawnMovesFromOffsets(const Piece &piece, Chessboard &board, std::vector<Move> &result, const BoardPosition &potential_pos);
+  BoardPosition m_normal_move_offset;
+  std::array<BoardPosition, 2> m_attack_move_offsets;
 };
 
 #endif // !__PAWN_MOVE_POLICY_HPP__
