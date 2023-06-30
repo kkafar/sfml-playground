576d5ab06ff9d1b77df7c5f163f049bc906752cb
Kacper Kafara <kacperkafara@gmail.com>
2023-06-30 12:48:18 +0200

    Add attack moves

--- src/Controller/Controller.hpp
+++ src/Controller/Controller.hpp
@@ -36,6 +36,7 @@ private:
   void focusPiece(Piece &piece);
   void blurPiece();
   void movePiece(Piece &piece, const BoardPosition &to_pos);
+  void removePiece(Piece &piece);
 
   bool positionIsInMoves(const BoardPosition &pos, const std::vector<Move> &moves) const;
 };
