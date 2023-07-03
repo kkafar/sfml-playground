d6997ffc55641059cb725c650edbc99850caaa56
Kacper Kafara <kacperkafara@gmail.com>
2023-07-03 16:39:05 +0200

    Calculate second move only if first one is valid (pawn)

--- src/Model/PawnMovePolicy.cpp
+++ src/Model/PawnMovePolicy.cpp
@@ -6,7 +6,8 @@
 #include <algorithm>
 #include <glog/logging.h>
 
-PawnMovePolicy::PawnMovePolicy(PlayerKind piece_color) : MovePolicy(PieceKind::Pawn), m_pawn_color(piece_color) {
+PawnMovePolicy::PawnMovePolicy(PlayerKind piece_color)
+    : MovePolicy(PieceKind::Pawn), m_pawn_color(piece_color) {
   if (m_pawn_color == PlayerKind::Black) {
     m_normal_move_offset = BoardPosition{-1, 0};
     m_attack_move_offsets[0] = BoardPosition{-1, -1};
@@ -24,19 +25,20 @@ void PawnMovePolicy::allMoves(const Piece &piece, Chessboard &board,
                               std::vector<Move> &result) {
   BoardPosition crt_pos = piece.position() + m_normal_move_offset;
   if (isPositionInChessboardBounds(crt_pos) && board.isTileEmpty(crt_pos)) {
-    result.push_back(Move { crt_pos, Move::Kind::Normal });
-  }
+    result.push_back(Move{crt_pos, Move::Kind::Normal});
 
-  crt_pos += m_normal_move_offset;
-  if (isInInitialRow(piece) && isPositionInChessboardBounds(crt_pos) && board.isTileEmpty(crt_pos)) {
-    result.push_back(Move { crt_pos, Move::Kind::Normal });
+    crt_pos += m_normal_move_offset;
+    if (isInInitialRow(piece) && isPositionInChessboardBounds(crt_pos) &&
+        board.isTileEmpty(crt_pos)) {
+      result.push_back(Move{crt_pos, Move::Kind::Normal});
+    }
   }
 
   for (const BoardPosition &offset : m_attack_move_offsets) {
     crt_pos = piece.position() + offset;
     if (isPositionInChessboardBounds(crt_pos) && !board.isTileEmpty(crt_pos)) {
       if (board.getPieceAt(crt_pos)->get().color() != piece.color()) {
-        result.push_back(Move { crt_pos, Move::Kind::Attack });
+        result.push_back(Move{crt_pos, Move::Kind::Attack});
       }
     }
   }
